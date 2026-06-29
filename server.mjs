66694613dc928a09a2e5d3e720fd3251c0389737
Marius Hansen <mariuswha@outlook.com>
2026-06-29 23:00:19 +0200

    Fix2333

--- server.mjs
+++ server.mjs
@@ -7,7 +7,7 @@ import { getL10n } from "./server/services/getL10n.mjs";
 import { getUsers } from "./modules/storageProviderSQL.mjs";
 import exportRouter from "./server/server-routes/settingsAPI.mjs";
 import getAdminUsers from "./server/server-routes/adminGetUser.mjs";
-const PORT = process.env.port; 
+const PORT = 5432; 
 const app = express();
 
 
